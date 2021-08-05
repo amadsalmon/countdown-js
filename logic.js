dcdeb9ccf8686512b84c8dfc47994bda0352655b
Amad Salmon <amad.salmon@icloud.com>
2021-08-06 01:13:54 +0200

    Add support for setting date through URL parameters

--- logic.js
+++ logic.js
@@ -2,14 +2,23 @@
 const today = new Date();
 const nextNewYear = new Date(today.getFullYear() + 1, 0, 1);
 var countDownDate = nextNewYear;
+var countdownTitle = "Next New Year";
 
 const url_string = window.location.search;
 console.log(url_string);
 if (url_string != "") {
     const urlParams = new URLSearchParams(url_string);
-    // Does nothing (for now)
+    const dateParam = urlParams.get("date");
+    countdownTitle = urlParams.get("title") ?? '';
+
+    if (dateParam) {
+        countDownDate = new Date(dateParam);
+    }
+
+    }
 }
 
+document.getElementById("countdownTitle").innerHTML = countdownTitle;
 // Update the count down every 1 second
 var x = setInterval(function () {
     // Get today's date and time
@@ -26,9 +35,10 @@ var x = setInterval(function () {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Output the result in an element with id="countdown"
-    document.getElementById("countdownValue").innerHTML =
-        days + "j " + hours + "h " + minutes + "m " + seconds + "s ";
+    // Output the result
+    document.getElementById(
+        "countdownValue"
+    ).innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
     // If the count down is over, write some text
     if (distance < 0) {
